66131704c7b9f956ce2e96a5d67afd6a9ab519cd
Dioniss97 <dioniss1997@gmail.com>
2022-05-17 16:00:31 +0200

    blade momento

--- public/front/desktop/js/app.js
+++ public/front/desktop/js/app.js
@@ -88,6 +88,41 @@ var renderAmount = function renderAmount() {
 
 /***/ }),
 
+/***/ "./resources/js/front/desktop/app.js":
+/*!*******************************************!*\
+  !*** ./resources/js/front/desktop/app.js ***!
+  \*******************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _hamburger_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./hamburger.js */ "./resources/js/front/desktop/hamburger.js");
+/* harmony import */ var _accordion_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./accordion.js */ "./resources/js/front/desktop/accordion.js");
+/* harmony import */ var _ckeditor_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./ckeditor.js */ "./resources/js/front/desktop/ckeditor.js");
+/* harmony import */ var _amount_button_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./amount-button.js */ "./resources/js/front/desktop/amount-button.js");
+/* harmony import */ var _notification_js__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./notification.js */ "./resources/js/front/desktop/notification.js");
+/* harmony import */ var _product_js__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./product.js */ "./resources/js/front/desktop/product.js");
+/* harmony import */ var _select_tabs__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./select-tabs */ "./resources/js/front/desktop/select-tabs.js");
+/* harmony import */ var _tabs_js__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./tabs.js */ "./resources/js/front/desktop/tabs.js");
+
+
+
+
+
+
+
+
+(0,_accordion_js__WEBPACK_IMPORTED_MODULE_1__.renderAccordion)();
+(0,_amount_button_js__WEBPACK_IMPORTED_MODULE_3__.renderAmount)();
+(0,_ckeditor_js__WEBPACK_IMPORTED_MODULE_2__.renderCkeditor)();
+(0,_hamburger_js__WEBPACK_IMPORTED_MODULE_0__.renderHamburger)();
+(0,_notification_js__WEBPACK_IMPORTED_MODULE_4__.renderNotification)();
+(0,_product_js__WEBPACK_IMPORTED_MODULE_5__.renderProduct)();
+(0,_select_tabs__WEBPACK_IMPORTED_MODULE_6__.renderSelectTabs)();
+(0,_tabs_js__WEBPACK_IMPORTED_MODULE_7__.renderTabs)();
+
+/***/ }),
+
 /***/ "./resources/js/front/desktop/ckeditor.js":
 /*!************************************************!*\
   !*** ./resources/js/front/desktop/ckeditor.js ***!
@@ -270,6 +305,58 @@ var renderTabs = function renderTabs() {
   }
 };
 
+/***/ }),
+
+/***/ "./resources/sass/front/app.scss":
+/*!***************************************!*\
+  !*** ./resources/sass/front/app.scss ***!
+  \***************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./resources/sass/front/app-mobile.scss":
+/*!**********************************************!*\
+  !*** ./resources/sass/front/app-mobile.scss ***!
+  \**********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./resources/sass/admin/app.scss":
+/*!***************************************!*\
+  !*** ./resources/sass/admin/app.scss ***!
+  \***************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./resources/sass/admin/app-mobile.scss":
+/*!**********************************************!*\
+  !*** ./resources/sass/admin/app-mobile.scss ***!
+  \**********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
 /***/ })
 
 /******/ 	});
@@ -301,7 +388,42 @@ var renderTabs = function renderTabs() {
 /******/ 		return module.exports;
 /******/ 	}
 /******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/chunk loaded */
+/******/ 	(() => {
+/******/ 		var deferred = [];
+/******/ 		__webpack_require__.O = (result, chunkIds, fn, priority) => {
+/******/ 			if(chunkIds) {
+/******/ 				priority = priority || 0;
+/******/ 				for(var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
+/******/ 				deferred[i] = [chunkIds, fn, priority];
+/******/ 				return;
+/******/ 			}
+/******/ 			var notFulfilled = Infinity;
+/******/ 			for (var i = 0; i < deferred.length; i++) {
+/******/ 				var [chunkIds, fn, priority] = deferred[i];
+/******/ 				var fulfilled = true;
+/******/ 				for (var j = 0; j < chunkIds.length; j++) {
+/******/ 					if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every((key) => (__webpack_require__.O[key](chunkIds[j])))) {
+/******/ 						chunkIds.splice(j--, 1);
+/******/ 					} else {
+/******/ 						fulfilled = false;
+/******/ 						if(priority < notFulfilled) notFulfilled = priority;
+/******/ 					}
+/******/ 				}
+/******/ 				if(fulfilled) {
+/******/ 					deferred.splice(i--, 1)
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
+/******/ 				}
+/******/ 			}
+/******/ 			return result;
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -363,40 +485,74 @@ var renderTabs = function renderTabs() {
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	(() => {
+/******/ 		// no baseURI
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"/front/desktop/js/app": 0,
+/******/ 			"admin/mobile/css/app-mobile": 0,
+/******/ 			"admin/desktop/css/app": 0,
+/******/ 			"front/mobile/css/app-mobile": 0,
+/******/ 			"front/desktop/css/app": 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		__webpack_require__.O.j = (chunkId) => (installedChunks[chunkId] === 0);
+/******/ 		
+/******/ 		// install a JSONP callback for chunk loading
+/******/ 		var webpackJsonpCallback = (parentChunkLoadingFunction, data) => {
+/******/ 			var [chunkIds, moreModules, runtime] = data;
+/******/ 			// add "moreModules" to the modules object,
+/******/ 			// then flag all "chunkIds" as loaded and fire callback
+/******/ 			var moduleId, chunkId, i = 0;
+/******/ 			if(chunkIds.some((id) => (installedChunks[id] !== 0))) {
+/******/ 				for(moduleId in moreModules) {
+/******/ 					if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 						__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 					}
+/******/ 				}
+/******/ 				if(runtime) var result = runtime(__webpack_require__);
+/******/ 			}
+/******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
+/******/ 			for(;i < chunkIds.length; i++) {
+/******/ 				chunkId = chunkIds[i];
+/******/ 				if(__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 					installedChunks[chunkId][0]();
+/******/ 				}
+/******/ 				installedChunks[chunkId] = 0;
+/******/ 			}
+/******/ 			return __webpack_require__.O(result);
+/******/ 		}
+/******/ 		
+/******/ 		var chunkLoadingGlobal = self["webpackChunk"] = self["webpackChunk"] || [];
+/******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
+/******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	})();
+/******/ 	
 /************************************************************************/
-var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be in strict mode.
-(() => {
-"use strict";
-/*!*******************************************!*\
-  !*** ./resources/js/front/desktop/app.js ***!
-  \*******************************************/
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _hamburger_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./hamburger.js */ "./resources/js/front/desktop/hamburger.js");
-/* harmony import */ var _accordion_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./accordion.js */ "./resources/js/front/desktop/accordion.js");
-/* harmony import */ var _ckeditor_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./ckeditor.js */ "./resources/js/front/desktop/ckeditor.js");
-/* harmony import */ var _amount_button_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./amount-button.js */ "./resources/js/front/desktop/amount-button.js");
-/* harmony import */ var _notification_js__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./notification.js */ "./resources/js/front/desktop/notification.js");
-/* harmony import */ var _product_js__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./product.js */ "./resources/js/front/desktop/product.js");
-/* harmony import */ var _select_tabs__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./select-tabs */ "./resources/js/front/desktop/select-tabs.js");
-/* harmony import */ var _tabs_js__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./tabs.js */ "./resources/js/front/desktop/tabs.js");
-
-
-
-
-
-
-
-
-(0,_accordion_js__WEBPACK_IMPORTED_MODULE_1__.renderAccordion)();
-(0,_amount_button_js__WEBPACK_IMPORTED_MODULE_3__.renderAmount)();
-(0,_ckeditor_js__WEBPACK_IMPORTED_MODULE_2__.renderCkeditor)();
-(0,_hamburger_js__WEBPACK_IMPORTED_MODULE_0__.renderHamburger)();
-(0,_notification_js__WEBPACK_IMPORTED_MODULE_4__.renderNotification)();
-(0,_product_js__WEBPACK_IMPORTED_MODULE_5__.renderProduct)();
-(0,_select_tabs__WEBPACK_IMPORTED_MODULE_6__.renderSelectTabs)();
-(0,_tabs_js__WEBPACK_IMPORTED_MODULE_7__.renderTabs)();
-})();
-
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
+/******/ 	__webpack_require__.O(undefined, ["admin/mobile/css/app-mobile","admin/desktop/css/app","front/mobile/css/app-mobile","front/desktop/css/app"], () => (__webpack_require__("./resources/js/front/desktop/app.js")))
+/******/ 	__webpack_require__.O(undefined, ["admin/mobile/css/app-mobile","admin/desktop/css/app","front/mobile/css/app-mobile","front/desktop/css/app"], () => (__webpack_require__("./resources/sass/front/app.scss")))
+/******/ 	__webpack_require__.O(undefined, ["admin/mobile/css/app-mobile","admin/desktop/css/app","front/mobile/css/app-mobile","front/desktop/css/app"], () => (__webpack_require__("./resources/sass/front/app-mobile.scss")))
+/******/ 	__webpack_require__.O(undefined, ["admin/mobile/css/app-mobile","admin/desktop/css/app","front/mobile/css/app-mobile","front/desktop/css/app"], () => (__webpack_require__("./resources/sass/admin/app.scss")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["admin/mobile/css/app-mobile","admin/desktop/css/app","front/mobile/css/app-mobile","front/desktop/css/app"], () => (__webpack_require__("./resources/sass/admin/app-mobile.scss")))
+/******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
+/******/ 	
 /******/ })()
 ;
--- resources/views/front/pages/product/product.blade.php
+++ resources/views/front/pages/product/product.blade.php
@@ -5,61 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="css/style.css"> -->
-    <link rel="stylesheet" href="css/style-mobile.css">
+    @include('front.layout.partials.styles')
     <title>Hobby Expert</title>
 </head>
 
 <body>
-    <header>
-        <div class="index menu mobile-only" id="menu">
-            <ul>
-                <li><a href="index.html">inicio</a></li>
-                <li><a href="products.html">tienda</a></li>
-                <li><a href="cart.html">carrito</a></li>
-                <li><a href="contact.html">contacto</a></li>
-            </ul>
-        </div>
-        <div class="desktop-two-columns">
-            <div class="column">
-                <div class="header-logo">
-                    <div class="desktop-two-columns">
-                        <div class="column">
-                            <div class="header-logo-img">
-                                <img src="img/AK47.svg" alt="ak47">
-                            </div>
-                            <div class="header-hamburger mobile-only">
-                                <div class="index hamburger " id="hamburger">
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="column">
-                            <div class="header-logo-title">
-                                <h1>Hobby Expert</h1>
-                            </div>
-                            <div class="header-logo-subtitle">
-                                <h3>Distribuidor de equipación y réplicas de Airsoft</h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="column">
-                <div class="header-menu desktop-only">
-                    <ul>
-                        <li><a href="index.html">inicio</a></li>
-                        <li><a href="products.html">tienda</a></li>
-                        <li><a href="">servicios</a></li>
-                        <li><a href="contact.html">contacto</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </header>
-
+    @include('front.layout.partials.header')
     <main>
         <div class="product-view">
             <div class="desktop-two-columns">
@@ -215,30 +166,7 @@
         </div>
         </div>
     </main>
-
-    <footer>
-        <div class="desktop-two-columns">
-            <div class="column">
-                <ul>
-                    <li>footer</li>
-                    <li>footer</li>
-                    <li>footer</li>
-                    <li>footer</li>
-                    <li>footer</li>
-                    <li>footer</li>
-                </ul>
-            </div>
-            <div class="column">
-                <div class="mapouter">
-                    <div class="gmap_canvas">
-                        <iframe width="500" height="400" id="gmap_canvas"
-                            src="https://maps.google.com/maps?q=Black%20Shadow&t=k&z=9&ie=UTF8&iwloc=&output=embed"
-                            frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <script type="module" src="dist/app.js"></script>
+    @include('front.layout.partials.footer')
+    @include('front.layout.partials.js')
 </body>
 </html>
